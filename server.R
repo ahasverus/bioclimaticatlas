fab9b089d5a67f353b9dd6ccd76e398accad7ec3
Nicolas Casajus <nicolas.casajus@gmail.com>
2018-02-21 09:51:51 -0500

    Add climate change interface

--- server.R
+++ server.R
@@ -6,36 +6,77 @@ library(raster)
 library(rgdal)
 library(RColorBrewer)
 
-setwd("~/Desktop/")
+# setwd("~/Desktop/")
 
 
-species <- readRDS("bioclimaticatlas/data/species_list.rds")
-grd     <- readRDS("bioclimaticatlas/data/grid.rds")
+species <- readRDS("data/species_list.rds")
+climate <- readRDS("data/variables_list.rds")
+grd     <- readRDS("data/grid.rds")
 
 server <- function(input, output, session) {
 
-  output$map <- renderLeaflet({
+  output$map_climate <- renderLeaflet({
 
-    map <- leaflet(
-      data = grd
-    ) %>%
-    addProviderTiles(
-      provider = providers$Esri.WorldTopoMap
-    )
+    map_climate <- leaflet(data = grd) %>%
+    addProviderTiles(provider = providers$Esri.WorldTopoMap)
   })
 
   observe({
 
-
     ### Update map
     leafletProxy(
-      mapId = "map",
-      data  = grd) %>%
+      mapId = "map_climate",
+      data  = grd
+    ) %>%
     fitBounds(
       lng1 = ~-45.50,
       lat1 = ~ 50.00,
       lng2 = ~-75.50,
       lat2 = ~ 63.00
     )
+  })
+
+  output$map_species <- renderLeaflet({
+
+    map_species <- leaflet(data = grd) %>%
+    addProviderTiles(provider = providers$Esri.WorldTopoMap)
+  })
+
+  observe({
+
+    ### Update map
+    leafletProxy(
+      mapId = "map_species",
+      data  = grd
+    ) %>%
+    fitBounds(
+      lng1 = ~-45.50,
+      lat1 = ~ 50.00,
+      lng2 = ~-75.50,
+      lat2 = ~ 63.00
+    )
+  })
+
+  onclick("nav",
+
+    for (i in c("map_climate", "map_species")) {
+
+      leafletProxy(
+        mapId = i,
+        data  = grd
+      ) %>%
+
+      fitBounds(
+        lng1 = ~-45.50,
+        lat1 = ~ 50.00,
+        lng2 = ~-75.50,
+        lat2 = ~ 63.00
+      )
+    }
+  )
+
+  observe({
+    if (input$nav == "Climate change")
+      showNotification(input$nav)
   })
 }
--- ui.R
+++ ui.R
@@ -6,28 +6,41 @@ library(raster)
 library(rgdal)
 library(RColorBrewer)
 
-setwd("~/Desktop/")
+
+# setwd("~/Desktop/bioclimaticatlas")
 
 
-species <- readRDS("bioclimaticatlas/data/species_list.rds")
-grd     <- readRDS("bioclimaticatlas/data/grid.rds")
+species <- readRDS("data/species_list.rds")
+climate <- readRDS("data/variables_list.rds")
+grd     <- readRDS("data/grid.rds")
 
 
 
 ui <- navbarPage(
 
-  title = "Tundra Nunavik",
-  id    = "nav",
+  title       = "Tundra Nunavik",
+  id          = "nav",
   collapsible = TRUE,
 
-  tabPanel("Climate change",
+
+  ### HOME PANEL --------------------------------
+
+  tabPanel(icon("home"),
+
+    HTML("Work in progress...")
+  ),
+
+
+  ### CLIMATE CHANGE PANEL ----------------------
+
+  tabPanel("Climate change", id = "climate-change",
 
     div(class = "outer",
 
+      useShinyjs(),
+
       tags$head(
-        includeCSS("css/style.css")
-      ),
-      tags$head(
+        includeCSS("css/style.css"),
         tags$link(
           rel  = "stylesheet",
           type = "text/css",
@@ -35,12 +48,14 @@ ui <- navbarPage(
         )
       ),
 
-      # If not using custom CSS, set height of leafletOutput to a number instead of percent
-      leafletOutput(outputId = "map", width = "100%", height = "100%"),
+      leafletOutput(
+        outputId = "map_climate",
+        width    = "100%",
+        height   = "100%"
+      ),
 
-      # Shiny versions prior to 0.11 should use class = "modal" instead.
       absolutePanel(
-        id        = "controls",
+        id        = "controls_climate",
         class     = "panel panel-default",
         fixed     = TRUE,
         draggable = TRUE,
@@ -51,144 +66,151 @@ ui <- navbarPage(
         width     = 340,
         height    = "auto",
 
-        HTML('<h4>Navigation menu</h4>'),
-        HTML('<hr />'),
+        HTML('<h4>Climate change interface</h4><hr />'),
+
         radioButtons(
-          inputId  = "searchby",
-          label    = "Search species by",
-          choices  = list("English name" = "common_en", "French name" = "common_fr", "Scientific name" = "latin", "Inuktitut" = "inuktitut"),
-          selected = "latin",
+          inputId  = "searchby_climate",
+          label    = "Select the language:",
+          choices  = list("English" = "english", "French" = "french"),
+          selected = "english",
           inline   = FALSE,
           width    = 300
         ),
-        radioButtons(
-          inputId  = "spclass",
-          label    = "Select the species",
-          choices  = list("Birds" = "Aves", "Mammals" = "Mammalia"),
-          selected = "Aves",
-          inline   = FALSE,
-          width    = 300
-        ),
+
         selectInput(
-          inputId = "species",
-          label   = NULL,
-          choices = sort(unique(as.character(species[, "latin"])))
+          inputId = "species_climate",
+          label   = "Select the variable:",
+          choices = sort(unique(as.character(climate[, "english"])))
         ),
+
         radioButtons(
-          inputId  = "horizon",
-          label    = "Select the horizon",
+          inputId  = "horizon_climate",
+          label    = "Select the horizon:",
           choices  = c("1981-2010", "2011-2040", "2041-2070", "2071-2100"),
           selected = "1981-2010",
           inline   = FALSE,
           width    = 300
         ),
+
         radioButtons(
-          inputId  = "scenario",
-          label    = "Select the RCP",
+          inputId  = "scenario_climate",
+          label    = "Select the RCP:",
           choices  = c("RCP4.5", "RCP8.5"),
           selected = character(0),
           inline   = FALSE,
           width    = 300
         ),
+
         radioButtons(
-          inputId  = "infos",
-          label    = "Information to be displayed",
+          inputId  = "infos_climate",
+          label    = "Information to be displayed:",
+          choices  = c("Climate normals", "Uncertainties", "Anomalies"),
+          selected = "Climate normals",
+          inline   = FALSE,
+          width    = 300
+        ),
+
+        HTML('<hr /><a href="#" id="btn-climate" class="btn-png"><i class="fa fa-download"></i>Download as PNG</a>')
+      )
+    )
+  ),
+
+
+  ### SPECIES DISTRIBUTION PANEL ----------------
+
+  tabPanel("Species distribution", id = "species-distribution",
+
+    div(class = "outer",
+
+      leafletOutput(
+        outputId = "map_species",
+        width    = "100%",
+        height   = "100%"
+      ),
+
+      absolutePanel(
+        id        = "controls_species",
+        class     = "panel panel-default",
+        fixed     = TRUE,
+        draggable = TRUE,
+        top       = 60,
+        left      = "auto",
+        right     = 5,
+        bottom    = "auto",
+        width     = 340,
+        height    = "auto",
+
+        HTML('<h4>Species distribution interface</h4><hr />'),
+
+        radioButtons(
+          inputId  = "searchby_species",
+          label    = "Search species by:",
+          choices  = list("English name" = "common_en", "French name" = "common_fr", "Scientific name" = "latin", "Inuktitut" = "inuktitut"),
+          selected = "latin",
+          inline   = FALSE,
+          width    = 300
+        ),
+
+        radioButtons(
+          inputId  = "spclass_species",
+          label    = "Select the species:",
+          choices  = list("Birds" = "Aves", "Mammals" = "Mammalia"),
+          selected = "Aves",
+          inline   = FALSE,
+          width    = 300
+        ),
+
+        selectInput(
+          inputId = "species_species",
+          label   = NULL,
+          choices = sort(unique(as.character(species[, "latin"])))
+        ),
+
+        radioButtons(
+          inputId  = "horizon_species",
+          label    = "Select the horizon:",
+          choices  = c("1981-2010", "2011-2040", "2041-2070", "2071-2100"),
+          selected = "1981-2010",
+          inline   = FALSE,
+          width    = 300
+        ),
+
+        radioButtons(
+          inputId  = "scenario_species",
+          label    = "Select the RCP:",
+          choices  = c("RCP4.5", "RCP8.5"),
+          selected = character(0),
+          inline   = FALSE,
+          width    = 300
+        ),
+
+        radioButtons(
+          inputId  = "infos_species",
+          label    = "Information to be displayed:",
           choices  = c("Observations", "Binaries", "Probabilities", "Uncertainties"),
           selected = "Observations",
           inline   = FALSE,
           width    = 300
         ),
-        HTML('<hr />'),
-          # a('Download as PNG', href = "#", icon = icon("download")),
-        HTML('<a href="#" class="btn-png"><i class="fa fa-download"></i>Download as PNG</a>')#,
-        # actionButton("xxx", 'Download as PNG', icon = icon("download"))
+
+        HTML('<hr /><a href="#" id="btn-species" class="btn-png"><i class="fa fa-download"></i>Download as PNG</a>')
       )
     )
   ),
-  tabPanel("Species distribution"),
+
+
+  ### ECOSYSTEM CHANGES PANEL -------------------
+
   tabPanel("Ecosystem changes",
+
     HTML("Work in progress...")
   ),
-  tabPanel("About",
+
+
+  ### ABOUT PANEL --------------------------------
+
+  tabPanel("Data access",
+
     HTML("Work in progress...")
   )
 )
-
-#
-# ui <- tagList(
-#
-#   includeCSS("css/style.css"),
-#
-#     navbarPage("Tundra Nunavik", id = "nav",                 # Brand title
-#
-#     tabPanel("Climate change",                               # Tab 1 title
-#
-#       titlePanel("Visualize climate change"),                # Page 1 title
-#
-#       column(width = 3,                                      # Form sidebar
-#
-#         wellPanel(
-#
-#           HTML('<h4>Navigation menu</h4>'),
-#           # HTML('<hr /><div class="pad"></div>'),
-#           HTML('<hr />'),
-#           radioButtons(
-#             inputId  = "searchby",
-#             label    = "Search species by",
-#             choices  = list("English name" = "common_en", "French name" = "common_fr", "Scientific name" = "latin"),
-#             selected = "latin",
-#             inline   = FALSE,
-#             width    = 250
-#           ),
-#           radioButtons(
-#             inputId  = "spclass",
-#             label    = "Select the species",
-#             choices  = list("Birds" = "Aves", "Mammals" = "Mammalia"),
-#             selected = "Aves",
-#             inline   = FALSE,
-#             width    = 250
-#           ),
-#           selectInput(
-#             inputId = "species",
-#             label   = NULL,
-#             choices = sort(unique(as.character(species[, "latin"])))
-#           ),
-#           radioButtons(
-#             inputId  = "horizon",
-#             label    = "Select the horizon",
-#             choices  = c("1981-2010", "2011-2040", "2041-2070", "2071-2100"),
-#             selected = "1981-2010",
-#             inline   = FALSE,
-#             width    = 250
-#           ),
-#           radioButtons(
-#             inputId  = "scenario",
-#             label    = "Select the RCP",
-#             choices  = c("RCP4.5", "RCP8.5"),
-#             selected = character(0),
-#             inline   = FALSE,
-#             width    = 250
-#           ),
-#           radioButtons(
-#             inputId  = "infos",
-#             label    = "Information to be displayed",
-#             choices  = c("Observations", "Binaries", "Probabilities", "Uncertainties"),
-#             selected = "Observations",
-#             inline   = FALSE,
-#             width    = 250
-#           )#,
-#           # HTML('<hr /><div class="pad"></div>'),
-#         )
-#       ),
-#
-#       column(width = 9,
-#
-#         leafletOutput(
-#           outputId = "map",
-#           # width    = "98.0%",
-#           height   = "560"
-#         )
-#       )
-#     )
-#   )
-# )
