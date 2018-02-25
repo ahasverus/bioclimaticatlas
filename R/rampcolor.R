63c9b731d69aa217eb3509483fe462b7398a3b19
Nicolas Casajus <nicolas.casajus@gmail.com>
2018-02-25 11:24:16 -0500

    Convert in R function

--- R/rampcolor.R
+++ R/rampcolor.R
@@ -1,42 +1,43 @@
-rampcolors <- data.frame(
-  palette          = rownames(brewer.pal.info),
-  maxcolors        = brewer.pal.info[ , "maxcolors"],
-  stringsAsFactors = FALSE
-)
+addCSSHTML <- function() {
 
-css  <- ""
-html <- ""
-
-
-for (i in 1:nrow(rampcolors)) {
-
-  cols <- brewer.pal(name = rampcolors[i, "palette"], n = rampcolors[i, "maxcolors"])
-  cols <- tolower(cols)
-
-  css  <- paste0(css,
-    ".", rampcolors[i, "palette"], " {\n",
-    "  background: ", cols[length(cols)], ";\n",
-    "  background: linear-gradient(to right, ", paste0(cols, collapse = ", "), ");\n",
-    "}\n\n"
+  rampcolors <- data.frame(
+    palette          = rownames(brewer.pal.info),
+    maxcolors        = brewer.pal.info[ , "maxcolors"],
+    stringsAsFactors = FALSE
   )
 
-  html <- paste0(html,
-    "    <div class=\"", rampcolors[i, "palette"], " color-list\"></div>\n"
-  )
+  css  <- html <- ""
 
-  cols <- cols[length(cols):1]
+  for (i in 1:nrow(rampcolors)) {
 
-  css  <- paste0(css,
-    ".", rampcolors[i, "palette"], "-rev {\n",
-    "  background: ", cols[length(cols)], ";\n",
-    "  background: linear-gradient(to right, ", paste0(cols, collapse = ", "), ");\n",
-    "}\n\n"
-  )
+    cols <- brewer.pal(name = rampcolors[i, "palette"], n = rampcolors[i, "maxcolors"])
+    cols <- tolower(cols)
 
-  html <- paste0(html,
-    "    <div class=\"", rampcolors[i, "palette"], "-rev color-list\"></div>\n"
-  )
+    css  <- paste0(css,
+      ".", rampcolors[i, "palette"], " {\n",
+      "  background: ", cols[length(cols)], ";\n",
+      "  background: linear-gradient(to right, ", paste0(cols, collapse = ", "), ");\n",
+      "}\n\n"
+    )
+
+    html <- paste0(html,
+      "    <div class=\"", rampcolors[i, "palette"], " color-list\"></div>\n"
+    )
+
+    cols <- cols[length(cols):1]
+
+    css  <- paste0(css,
+      ".", rampcolors[i, "palette"], "-rev {\n",
+      "  background: ", cols[length(cols)], ";\n",
+      "  background: linear-gradient(to right, ", paste0(cols, collapse = ", "), ");\n",
+      "}\n\n"
+    )
+
+    html <- paste0(html,
+      "    <div class=\"", rampcolors[i, "palette"], "-rev color-list\"></div>\n"
+    )
+  }
+
+  cat(html, file = "includes/color-picker.html", append = FALSE)
+  cat(css, file = "css/color-gradients.css", append = FALSE)
 }
-
-cat(html, file = "includes/color-picker.html", append = FALSE)
-cat(css, file = "css/color-gradients.css", append = FALSE)
